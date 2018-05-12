f0d4231546efebcbaa16a850f3299cafca236022
papapin <papapin777#gmail.com>
2018-05-12 15:48:47 -0700

    first block

---
+++ sf-chain/block.js
@@ -0,0 +1,20 @@
+class Block {
+  constructor(timestamp,  lastHash, hash, data ){
+      this.timestamp = timestamp;
+      this.lastHash = lastHash;
+      this.hash = hash;
+      data.data = data;
+  }
+
+  toString() {
+    return ` Block -
+    Timestamp: ${this.timestamp}
+    lastHash:  ${this.lastHash.substring(0, 10)}
+    hash:      ${this.hash.substring(0, 10)}
+    data:      ${this.data}
+
+    `
+  }
+}
+
+module.exports = Block;
