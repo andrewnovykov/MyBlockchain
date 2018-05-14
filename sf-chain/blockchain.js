3662ceed4545673a94df50ac6c0027bb7aa60d1b
papapin <papapin777#gmail.com>
2018-05-13 17:38:41 -0700

    add blockchain class

---
+++ sf-chain/blockchain.js
@@ -0,0 +1,25 @@
+const Block = require('./block');
+
+
+class Blockchain {
+
+  constructor(){
+    this.chain = [Block.genesis()]
+    // console.log(this.chain);
+  }
+
+  addBlock(data){
+      const lastBlock = this.chain[this.chain.length - 1];
+      const block = Block.mineBlock(lastBlock, data);
+      this.chain.push(block);
+
+      return block;
+
+
+
+  }
+
+
+}
+
+module.exports = Blockchain;
